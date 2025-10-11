cd9c0be4d95648aa5dda07cf3effb08175f6839f
LC Perri <prluisca@gmail.com>
2025-10-11 00:11:38 -0500

    fixing vercel monitor, desinstalado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import CoinArea from '../components/CoinArea'
 import Footer from '../components/Footer'
-import BackButton from '../components/BackButton'
 
 const Home = () => {
   return (
